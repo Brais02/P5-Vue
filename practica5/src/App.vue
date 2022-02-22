42d1efe0f295937fee6258f65fae359113d75cb4
Brais <bvirlan@cifpfbmoll.eu>
2022-02-22 16:36:13 +0100

    Introduciendo el estilo en index

--- practica5/src/App.vue
+++ practica5/src/App.vue
@@ -1,8 +1,42 @@
 <template>
   <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link> |
-    <router-link to="/shop">Shop</router-link>
+    
+     <header class="navbar navbar-expand-lg navbar-dark bg-primary">
+
+      <img src="imagenes/logo.png" class="img-fluid shadow-lg ps-2">
+
+      <div class="centrarElemento">
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul class="navbar-nav text-center">
+            <li class="nav-item">
+              
+              <a class="nav-link active fw-bold"><router-link to="/">Home</router-link></a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link disabled fw-bold">About us</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link disabled fw-bold">Contact us</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link disabled fw-bold">Why choose us?</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link active fw-bold"><router-link to="/about">Blog</router-link></a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link active fw-bold"><router-link to="/shop">Shop</router-link></a>
+            </li>
+          </ul>
+
+        </div>
+      </div>
+    </header>
+    
   </div>
   <router-view/>
 </template>
--- practica5/src/components/HelloWorld.vue
+++ practica5/src/components/HelloWorld.vue
@@ -1,42 +1,253 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-router" target="_blank" rel="noopener">router</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-vuex" target="_blank" rel="noopener">vuex</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+    <div class="container">
+      <section class="d-grid gap-3">
+        <div class="card mt-3">
+          <div class="row d-flex">
+            <div class="col-md-12 col-lg-8 order-2 order-lg-1">
+              <article class="card-body">
+                <h2 class="card-title">Earth-friendly!</h2>
+                <p class="card-text justify">
+                  This website template has been designed by Free Website
+                  Templates for you, for free. You can replace all this text
+                  with your own text. You can remove any link to our website
+                  from this website template, you're free to use this website
+                  template without linking back to us. If you're having problems
+                  editing this website template, then don't hesitate to ask for
+                  help on the Forums.
+                </p>
+                <div class="d-flex justify-content-evenly">
+                  <button type="submit" class="btn btn-primary">
+                    Take a Tour
+                  </button>
+                  <button
+                    type="button"
+                    class="btn btn-primary position-relative"
+                  >
+                    Register
+                  </button>
+                </div>
+              </article>
+            </div>
+            <div class="col-md-12 col-lg-4 order-1 order-lg-2 d-flex">
+              <img
+                src="imagenes/Soplando.jpg"
+                class="
+                  align-self-center
+                  img-fluid
+                  rounded-start
+                  centrarElemento
+                  pe-lg-2
+                  shadow
+                "
+              />
+            </div>
+          </div>
+        </div>
+
+        <!-- Modal -->
+        <div
+          class="modal fade"
+          id="staticBackdrop"
+          data-bs-backdrop="static"
+          data-bs-keyboard="false"
+          tabindex="-1"
+          aria-labelledby="staticBackdropLabel"
+          aria-hidden="true"
+        >
+          <div class="modal-dialog">
+            <div class="modal-content">
+              <div class="modal-header">
+                <h5 class="modal-title" id="staticBackdropLabel">
+                  Modal title
+                </h5>
+                <button
+                  type="button"
+                  class="btn-close"
+                  data-bs-dismiss="modal"
+                  aria-label="Close"
+                ></button>
+              </div>
+              <div class="modal-body">...</div>
+              <div class="modal-footer">
+                <button
+                  type="button"
+                  class="btn btn-secondary"
+                  data-bs-dismiss="modal"
+                >
+                  Close
+                </button>
+                <button type="button" class="btn btn-primary">
+                  Understood
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <!-- CENTRAR ESTOS ELEMENTOS PARA QUE OCUPEN TODO EL ESPACIO-->
+        <div class="row g-0 gap-3 gap-md-0">
+          <div class="col-12 col-md-6 col-lg-4">
+            <article class="card">
+              <img src="imagenes/Mapa.jpg" class="card-img-top" alt="..." />
+              <div class="card-body">
+                <p class="card-text">
+                  Some quick example text to build on the card title and make up
+                  the bulk of the card's content.
+                </p>
+                <a href="#" class="btn btn-primary centrarElemento"
+                  >The Power of Earth</a
+                >
+              </div>
+            </article>
+          </div>
+
+          <div class="col-12 col-md-6 col-lg-4">
+            <article class="card">
+              <img src="imagenes/Abrazo.jpg" class="card-img-top" alt="..." />
+              <div class="card-body">
+                <p class="card-text">
+                  Some quick example text to build on the card title and make up
+                  the bulk of the card's content.
+                </p>
+                <a href="#" class="btn btn-primary centrarElemento"
+                  >Earth no mad</a
+                >
+              </div>
+            </article>
+          </div>
+
+          <div class="col-12 col-lg-4 mt-md-3 mt-lg-0">
+            <article class="card">
+              <img src="imagenes/Globo.jpg" class="card-img-top" alt="..." />
+              <div class="card-body">
+                <p class="card-text">
+                  Some quick example text to build on the card title and make up
+                  the bulk of the card's content.
+                </p>
+                <a href="#" class="btn btn-primary centrarElemento"
+                  >Osmosis Beats</a
+                >
+              </div>
+            </article>
+          </div>
+        </div>
+      </section>
+
+      <footer class="mt-3 justify">
+        <section class="row g-0 gap-3 gap-md-0">
+          <div
+            class="bg-primary col-md-4 border border-success rounded-2 border-3"
+          >
+            <img
+              src="imagenes/logo-footer.png"
+              class="img-fluid p-2 shadow-sm centrarElemento"
+            />
+            <article class="p-3">
+              <p>
+                This is just a place holder, so you can see what the site would
+                look like. This is just a place holder, so you can see what the
+                site would look like.
+              </p>
+              <p><b>Telephone:</b> 12345678-90</p>
+              <p><b>Fax:</b> 23456789-01</p>
+              <p><b>Email:</b> ask@ecothunder.com</p>
+            </article>
+          </div>
+
+          <div
+            class="bg-primary col-md-4 border border-success rounded-2 border-3"
+          >
+            <h3 class="text-center">Get Social with us!</h3>
+            <p class="p-3">
+              This is just a place holder, so you can see what the site would
+              look like. This is just a place holder, so you can see what the
+              site would look like.
+            </p>
+
+            <article class="d-flex justify-content-evenly pb-3 pb-md-0">
+              <a class="btn btn-secondary">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="16"
+                  height="16"
+                  fill="currentColor"
+                  class="bi bi-facebook"
+                  viewBox="0 0 16 16"
+                >
+                  <path
+                    d="M16 8.049c0-4.446-3.582-8.05-8-8.05C3.58 0-.002 3.603-.002 8.05c0 4.017 2.926 7.347 6.75 7.951v-5.625h-2.03V8.05H6.75V6.275c0-2.017 1.195-3.131 3.022-3.131.876 0 1.791.157 1.791.157v1.98h-1.009c-.993 0-1.303.621-1.303 1.258v1.51h2.218l-.354 2.326H9.25V16c3.824-.604 6.75-3.934 6.75-7.951z"
+                  />
+                </svg>
+              </a>
+              <a class="btn btn-secondary">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="16"
+                  height="16"
+                  fill="currentColor"
+                  class="bi bi-twitter"
+                  viewBox="0 0 16 16"
+                >
+                  <path
+                    d="M5.026 15c6.038 0 9.341-5.003 9.341-9.334 0-.14 0-.282-.006-.422A6.685 6.685 0 0 0 16 3.542a6.658 6.658 0 0 1-1.889.518 3.301 3.301 0 0 0 1.447-1.817 6.533 6.533 0 0 1-2.087.793A3.286 3.286 0 0 0 7.875 6.03a9.325 9.325 0 0 1-6.767-3.429 3.289 3.289 0 0 0 1.018 4.382A3.323 3.323 0 0 1 .64 6.575v.045a3.288 3.288 0 0 0 2.632 3.218 3.203 3.203 0 0 1-.865.115 3.23 3.23 0 0 1-.614-.057 3.283 3.283 0 0 0 3.067 2.277A6.588 6.588 0 0 1 .78 13.58a6.32 6.32 0 0 1-.78-.045A9.344 9.344 0 0 0 5.026 15z"
+                  />
+                </svg>
+              </a>
+              <a class="btn btn-secondary">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="16"
+                  height="16"
+                  fill="currentColor"
+                  class="bi bi-google"
+                  viewBox="0 0 16 16"
+                >
+                  <path
+                    d="M15.545 6.558a9.42 9.42 0 0 1 .139 1.626c0 2.434-.87 4.492-2.384 5.885h.002C11.978 15.292 10.158 16 8 16A8 8 0 1 1 8 0a7.689 7.689 0 0 1 5.352 2.082l-2.284 2.284A4.347 4.347 0 0 0 8 3.166c-2.087 0-3.86 1.408-4.492 3.304a4.792 4.792 0 0 0 0 3.063h.003c.635 1.893 2.405 3.301 4.492 3.301 1.078 0 2.004-.276 2.722-.764h-.003a3.702 3.702 0 0 0 1.599-2.431H8v-3.08h7.545z"
+                  />
+                </svg>
+              </a>
+            </article>
+          </div>
+
+          <div
+            class="bg-primary col-md-4 border border-success rounded-2 border-3"
+          >
+            <h3 class="text-center">Share your thoughts!</h3>
+            <article class="p-3">
+              <p>
+                This is just a place holder, so you can see what the site would
+                look like. This is just a place holder, so you can see what the
+                site would look like.
+              </p>
+              <form class="d-flex pb-3">
+                <input
+                  class="form-control me-2"
+                  type="search"
+                  placeholder="Search"
+                  aria-label="Search"
+                />
+                <button class="btn btn-info" type="submit">Search</button>
+              </form>
+              <p class="fw-bold">Copyright © 2011 Ecothunder Incorporated</p>
+              <p class="fw-bold fst-italic">
+                LRP 727 6783 83839 All rights reserved
+              </p>
+            </article>
+          </div>
+        </section>
+      </footer>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'HelloWorld',
+  name: "HelloWorld",
   props: {
-    msg: String
-  }
+    msg: String,
+  },
 }
 </script>
 
