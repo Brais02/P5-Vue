939a7ee0ca5d55194b8b92d4536df2a0438d19be
Brais <bvirlan@cifpfbmoll.eu>
2022-02-22 16:37:37 +0100

    Solventando error de style

--- practica5/src/components/HelloWorld.vue
+++ practica5/src/components/HelloWorld.vue
@@ -251,7 +251,7 @@ export default {
 }
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
+<!-- Add "scoped" attribute to limit CSS to this component only 
 <style scoped>
 h3 {
   margin: 40px 0 0;
@@ -268,3 +268,4 @@ a {
   color: #42b983;
 }
 </style>
+-->
